333e9f27e4e3b18ccef2e8e524fb328e18b21656
erashu122 <kbitu8676@gmail.com>
2026-01-31 20:55:09 +0530

    first commit

--- Array/MyArrayList.java
+++ Array/MyArrayList.java
@@ -20,6 +20,7 @@ public class MyArrayList {
             System.out.println("element at "+i+ " index is "+arrayList.get(i));
         }
 
+        //Find the sum of all elements in an ArrayList.
         int sum=0;
         for (int i = 0; i < s; i++) {
             sum=sum+ arrayList.get(i);
@@ -27,6 +28,26 @@ public class MyArrayList {
         }
         System.out.println("the sum of element is : " + sum);
 
+
+        //Find the largest and smallest element in an ArrayList.
+
+        int larg=0;
+        for (int i = 0; i < s; i++) {
+            if (larg<= arrayList.get(i)){
+                larg=arrayList.get(i);
+            }
+
+        }
+        System.out.println("the large number is : "+larg);
+
+        int small=arrayList.get(0);
+        for (int i = 1; i < s; i++) {
+            if (small>= arrayList.get(i)){
+                small=arrayList.get(i);
+            }
+
+        }
+        System.out.println("the smallest number is : "+small);
 
     }
 }
