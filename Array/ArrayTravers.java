ec35fc1e8a1fdab541bc9ccfc58ac25cc1ec0c7c
erashu122 <kbitu8676@gmail.com>
2026-02-02 20:14:03 +0530

    first commit

--- Array/ArrayTravers.java
+++ Array/ArrayTravers.java
@@ -4,13 +4,15 @@ public class ArrayTravers {
     public static void main(String[] args) {
         ArrayTravers obj=new ArrayTravers();
 
-        int[] arr = {5, 10, 15};
+        int[] arr = {5, 10, 15,22,45,20,65,23};
         int n = arr.length;
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i]+" ");
         }
         System.out.println();
-        obj.sumOfArray(arr);
+//        obj.sumOfArray(arr);
+//        obj.SumOfEven(arr,n);
+        obj.maxElement(arr,n);
     }
     public void sumOfArray(int[] arr){
         int sum=0;
@@ -20,5 +22,26 @@ public class ArrayTravers {
         }
         System.out.println("sum :"+sum);
 
+    }
+    public void SumOfEven(int arr[],int n){
+        int esum=0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i]%2==0){
+                System.out.println("The even numbers are :"+arr[i]);
+                esum+=arr[i];
+            }
+        }
+        System.out.println("Sum of even number :"+esum);
+
+    }
+    //max element
+    public void maxElement(int []arr,int n){
+        int max=arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("The max Element in Array :"+max);
     }
 }
