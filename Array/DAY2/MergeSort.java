8dabb2556847701fb32dfb7d6cec51d42fcec0d4
erashu122 <kbitu8676@gmail.com>
2026-02-04 23:22:15 +0530

    first commit

---
+++ Array/DAY2/MergeSort.java
@@ -0,0 +1,73 @@
+package Array.DAY2;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // divide
+    public static void mergeSort(int[] arr, int left, int right) {
+
+        if (left < right) {
+
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);       // left half
+            mergeSort(arr, mid + 1, right);  // right half
+
+            merge(arr, left, mid, right);    // merge both halves
+        }
+    }
+
+    // merge
+    public static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // copy data
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        // merge back
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // copy remaining
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+}
