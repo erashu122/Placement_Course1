9075409e1f5cfefb5ddc335727c82cbfa9e9774c
erashu122 <kbitu8676@gmail.com>
2026-02-13 20:41:02 +0530

    first commit

--- OOPS/polymorphism/CompileTimePolymorphismDemo.java
+++ OOPS/polymorphism/CompileTimePolymorphismDemo.java
@@ -1,4 +1,25 @@
 package OOPS.polymorphism;
 
+class Calculator{
+    //Overloaded methods
+    int add(int a,int b){
+        System.out.println("int int ");
+        return a+b;
+    }
+    double add(double a,double b){
+        return a+b;
+    }
+    int add(int a,int b,int c){
+        return a+b+c;
+    }
+}
+
 public class CompileTimePolymorphismDemo {
+    static void main() {
+        Calculator obj=new Calculator();
+
+        System.out.println(obj.add(1,3));     //call int add(int a,int b)
+        System.out.println(obj.add(12.2,45.3));     //double add(double a,double b)
+        System.out.println(obj.add(12,34,56)); //int add(int a,int b,int c)
+    }
 }
