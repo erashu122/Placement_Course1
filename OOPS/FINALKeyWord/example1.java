db2c5b52066434ae53d0989b6dd6331e05e63657
erashu122 <kbitu8676@gmail.com>
2026-02-14 12:05:32 +0530

    first commit

---
+++ OOPS/FINALKeyWord/example1.java
@@ -0,0 +1,15 @@
+package OOPS.FINALKeyWord;
+
+
+class student{
+    final int a=13;
+    void Print(){
+        System.out.println("your final number is "+a);
+    }
+
+}
+public class example1 {
+    static void main() {
+
+    }
+}
