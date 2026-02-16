9b183c0f31f9c4bcdf8a3d91a6eba385e487e7ce
erashu122 <kbitu8676@gmail.com>
2026-02-16 18:26:16 +0530

    first commit

---
+++ OOPS/AccessModifire/SuperkeyWords.java
@@ -0,0 +1,9 @@
+package OOPS.AccessModifire;
+class Student{
+    void show(){
+        System.out.println("Student is a best feautue for the company :");
+    }
+}
+
+public class SuperkeyWords {
+}
