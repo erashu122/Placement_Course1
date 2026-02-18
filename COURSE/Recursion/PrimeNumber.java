85e185d9ad299ef9bd68b3718847694fc486db16
erashu122 <kbitu8676@gmail.com>
2026-02-18 23:08:50 +0530

    first commit

---
+++ COURSE/Recursion/PrimeNumber.java
@@ -0,0 +1,35 @@
+package COURSE.Recursion;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    static boolean primeNumber(int n,int i){
+        if (n<=1){
+            return false;
+        }
+        if (n==i){
+            return true;
+        }
+        if (n%i==0){
+            return false;
+        }
+        return primeNumber(n,i+1);
+    }
+
+    static void main() {
+        char ch='e';
+        while( ch=='e'){
+            Scanner sc=new Scanner(System.in);
+            System.out.print("Enter a number ");
+            int m=sc.nextInt();
+            boolean flage=primeNumber(m,2);
+            if (flage){
+                System.out.println("prime number");
+            }else {
+                System.out.println("it is not a prime number");
+            }
+
+        }
+
+    }
+}
