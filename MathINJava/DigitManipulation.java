d82148e9ee11387faab95d2a701f05dc813d90f3
erashu122 <kbitu8676@gmail.com>
2026-02-23 23:42:48 +0530

    first commit

--- MathINJava/DigitManipulation.java
+++ MathINJava/DigitManipulation.java
@@ -1,12 +1,22 @@
 package MathINJava;
 
 public class DigitManipulation {
+    //this is the solving the number of digit using
+    // formula and its time complexity is constant
     public void findDigitUsingFormula(int num){
+        if (num==0){
+            System.out.println("number of digit :"+1);
+            return;
+        }
+        if (num<0){
+            num=-1*num;
+        }
         int res=(int)Math.log10(num)+1;
         System.out.println("Number of Digit :"+res);
     }
+
     static void main() {
         DigitManipulation di=new DigitManipulation();
-        di.findDigitUsingFormula(12345);
+        di.findDigitUsingFormula(0);
     }
 }
